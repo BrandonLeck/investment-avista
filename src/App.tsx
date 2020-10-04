57928f1f9de1ec5f31ca343f32675fe5867e91cf
Brandon Lecklerh Solarte Melenje <brandonleck@mac.local>
2020-10-03 20:22:44 -0500

    conf (Investment-app): prettier, eslint, bootstrap and core-avista are configured.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import { Navbar } from 'core-avista/Navbar'
+
+const navItems = [
+  {
+    href: '#home',
+    description: 'Home',
+    current: true,
+  },
+  {
+    href: '#about',
+    description: 'About',
+    current: false,
+  },
+]
+function App() {
+  return <Navbar logo="Investment Avista" navItems={navItems} />
+}
+
+export default App
